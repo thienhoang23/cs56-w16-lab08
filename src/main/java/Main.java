45311e828ffcba80e1b73a0795e012c3b54c7276
Thien Hoang <thienhoang@umail.ucsb.edu>
2016-02-24 13:09:31 -0800

    Only database left

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,51 +19,54 @@ import org.jscience.physics.model.RelativisticModel;
 import org.jscience.physics.amount.Amount;
 
 public class Main {
-
-  public static void main(String[] args) {
-
-    port(Integer.valueOf(System.getenv("PORT")));
-    staticFileLocation("/public");
-
-    get("/hello", (req, res) -> {
-	    RelativisticModel.select();
-	    Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
-	    return "E=mc^2: 12 GeV = " + m.toString();
-	});
-    get("/ucsb", (req, res) -> "Go Gauchos");
-    get("/", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
-
-            return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());
-
-    get("/db", (req, res) -> {
-      Connection connection = null;
-      Map<String, Object> attributes = new HashMap<>();
-      try {
-        connection = DatabaseUrl.extract().getConnection();
-
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-        ArrayList<String> output = new ArrayList<String>();
-        while (rs.next()) {
-          output.add( "Read from DB: " + rs.getTimestamp("tick"));
-        }
-
-        attributes.put("results", output);
-        return new ModelAndView(attributes, "db.ftl");
-      } catch (Exception e) {
-        attributes.put("message", "There was an error: " + e);
-        return new ModelAndView(attributes, "error.ftl");
-      } finally {
-        if (connection != null) try{connection.close();} catch(SQLException e){}
-      }
-    }, new FreeMarkerEngine());
-
-  }
-
+    
+    public static void main(String[] args) {
+	
+	port(Integer.valueOf(System.getenv("PORT")));
+	staticFileLocation("/public");
+	
+	get("/hello", (req, res) -> {
+		RelativisticModel.select();
+		
+		String energy = System.getenv().get("ENERGY");
+		
+		Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
+		return "E=mc^2: " + energy + " = " + m.toString();
+	    });
+	get("/ucsb", (req, res) -> "Go Gauchos");
+	get("/", (request, response) -> {
+		Map<String, Object> attributes = new HashMap<>();
+		attributes.put("message", "Hello World!");
+		
+		return new ModelAndView(attributes, "index.ftl");
+	    }, new FreeMarkerEngine());
+	
+	get("/db", (req, res) -> {
+		Connection connection = null;
+		Map<String, Object> attributes = new HashMap<>();
+		try {
+		    connection = DatabaseUrl.extract().getConnection();
+		    
+		    Statement stmt = connection.createStatement();
+		    stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+		    stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+		    ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+		    
+		    ArrayList<String> output = new ArrayList<String>();
+		    while (rs.next()) {
+			output.add( "Read from DB: " + rs.getTimestamp("tick"));
+		    }
+		    
+		    attributes.put("results", output);
+		    return new ModelAndView(attributes, "db.ftl");
+		    } catch (Exception e) {
+		    attributes.put("message", "There was an error: " + e);
+		    return new ModelAndView(attributes, "error.ftl");
+		} finally {
+		    if (connection != null) try{connection.close();} catch(SQLException e){}
+		}
+	    }, new FreeMarkerEngine());
+	
+    }
+    
 }
